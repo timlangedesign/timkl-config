31c388d0ac77dd06ce898e344c1db210c64c67e0
Tim Kjaer Lange <tim.k.lange@gmail.com>
2024-10-18 14:49:30 +0800

    Fixed editor color.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("timkl.options")
+
+vim.cmd("highlight Normal guibg=none")
+vim.cmd("highlight NonText guibg=none")
+
+vim.cmd([[
+  highlight TelescopeNormal guibg=none
+  highlight TelescopeBorder guibg=none
+  highlight TelescopePromptNormal guibg=none
+  highlight TelescopePromptBorder guibg=none
+  highlight TelescopeResultsNormal guibg=none
+  highlight TelescopeResultsBorder guibg=none
+  highlight TelescopePreviewNormal guibg=none
+  highlight TelescopePreviewBorder guibg=none
+]])
