a5c283b7c957946afe19fea5b28bf499af9c6393
Tim Kjaer Lange <tim.k.lange@gmail.com>
2024-10-18 15:33:54 +0800

    More color fixes.

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,3 +15,55 @@ vim.cmd([[
   highlight TelescopePreviewNormal guibg=none
   highlight TelescopePreviewBorder guibg=none
 ]])
+
+-- Set color scheme and transparency globally
+vim.o.background = "dark" -- Dark background
+
+-- Transparency for main window and splits
+vim.cmd([[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NonText guibg=NONE ctermbg=NONE
+  highlight NormalFloat guibg=NONE ctermbg=NONE
+  highlight NormalNC guibg=NONE ctermbg=NONE
+  highlight LineNr guibg=NONE ctermbg=NONE
+  highlight CursorLineNr guibg=NONE ctermbg=NONE
+]])
+
+-- Which-Key transparency
+vim.cmd([[
+  highlight WhichKeyFloat guibg=NONE ctermbg=NONE
+  highlight WhichKeyBorder guibg=NONE ctermbg=NONE
+  highlight NormalFloat guibg=NONE ctermbg=NONE
+  highlight FloatBorder guibg=NONE ctermbg=NONE
+]])
+
+-- File explorer transparency for NvimTree and NeoTree (depending on your setup)
+vim.cmd([[
+  highlight NvimTreeNormal guibg=NONE ctermbg=NONE
+  highlight NvimTreeEndOfBuffer guibg=NONE ctermbg=NONE
+  highlight NvimTreeNormalNC guibg=NONE ctermbg=NONE
+  highlight NeoTreeNormal guibg=NONE ctermbg=NONE
+  highlight NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+  highlight NeoTreeNormalNC guibg=NONE ctermbg=NONE
+]])
+
+-- Telescope transparency
+vim.cmd([[
+  highlight TelescopeNormal guibg=NONE ctermbg=NONE
+  highlight TelescopeBorder guibg=NONE ctermbg=NONE
+]])
+
+-- General floating windows (covering all possibilities for floating transparency)
+vim.cmd([[
+  highlight Pmenu guibg=NONE ctermbg=NONE
+  highlight PmenuSel guibg=NONE ctermbg=NONE
+  highlight FloatBorder guibg=NONE ctermbg=NONE
+]])
+
+vim.cmd([[
+  highlight CursorLine guibg=#31410A
+]])
+
+vim.cmd([[
+  highlight Visual guibg=#31410A
+]])
